02e55e408b1099a8c4b81723b64107fdf5abb4ae
Harshawardhan Gadgil <hgadgil@rbcon.com>
2013-04-16 11:18:40 -0700

    Added appenv and sinatra simple apps

---
+++ appenv/README.md
@@ -0,0 +1,13 @@
+appenv
+======
+
+Quick view of Environment variables for a CF user
+
+Usage:
+-----
+
+'''
+vmc push appenv --runtime ruby19
+'''
+
+Goto: http://appenv.BASE_URL
---
+++ sintra-simple/hello.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'sinatra'
+disable :protection
+
+get "/" do
+  "Hello World! - good grief this was hard! Let's put it down to me being a Sinatra noob!"
+end
